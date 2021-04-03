19ff5dae244bcd902f031a08f727cea16c16aec7
Joaquin Mamani <jmamani@grupoassa.com>
2021-04-03 20:06:09 -0300

    Carga imágenes para README

--- lib/responsive.dart
+++ lib/responsive.dart
@@ -4,7 +4,6 @@ bool isLandscape(BuildContext context) =>
     MediaQuery.of(context).orientation == Orientation.landscape;
 
 bool isTab(BuildContext context) {
-  print(MediaQuery.of(context).size.aspectRatio);
   return MediaQuery.of(context).size.aspectRatio >= 0.7 &&
       MediaQuery.of(context).size.aspectRatio < 1.6;
 }
